2efc23cd23d107fedfb00047e988bca647648e6f
AbdallaM16 <amoa98008@gmail.com>
2023-09-23 13:42:17 +0100

    first commit

---
+++ objects.js
@@ -0,0 +1,21 @@
+
+let student1 = {
+ firstName : "Abdalla",
+ lastName : "Moahmed",
+ age : 14
+}
+
+
+let student2 = {
+firstName : "Ahmed",
+ lastName : "Ali",
+ age : 12
+}
+
+// console.log(student1)
+// console.log(student2)
+
+
+
+console.log(student1.firstName)
+console.log(student2.firstName)
